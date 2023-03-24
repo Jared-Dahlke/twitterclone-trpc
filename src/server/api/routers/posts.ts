204d1858f11f4eed87e637fb8c1d116a4f71801c
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-24 13:32:02 -0700

    adds rate limiter on post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,8 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import { z } from "zod";
 
 import {
@@ -13,6 +15,12 @@ const filterUserForCient = (user: User) => ({
   id: user.id,
   username: user.username,
   profileImageUrl: user.profileImageUrl,
+});
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
 });
 
 export const postsRouter = createTRPCRouter({
@@ -53,6 +61,14 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+          message: "You are posting too fast. Please slow down.",
+        });
+      }
       const post = await ctx.prisma.post.create({
         data: {
           authorId,
